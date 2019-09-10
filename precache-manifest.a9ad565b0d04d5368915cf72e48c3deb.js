b4c51e30c99f0bae42a68e2e8d3e53e35ac13ce3
Natalia Dittren <natalia.dittren@gmail.com>
2019-09-10 17:50:10 -0400

    Updates

---
+++ precache-manifest.a9ad565b0d04d5368915cf72e48c3deb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773ccb5b50338b57d8c393351f19c19e",
+    "url": "/udemyproject/index.html"
+  },
+  {
+    "revision": "d2330b4bea31c6fa5f6a",
+    "url": "/udemyproject/static/css/main.f36fd746.chunk.css"
+  },
+  {
+    "revision": "430df5f5a713718980ac",
+    "url": "/udemyproject/static/js/2.296b3f3e.chunk.js"
+  },
+  {
+    "revision": "d2330b4bea31c6fa5f6a",
+    "url": "/udemyproject/static/js/main.41fa1a0a.chunk.js"
+  },
+  {
+    "revision": "e60a8baaa8a8fdafc8a8",
+    "url": "/udemyproject/static/js/runtime~main.9c135f23.js"
+  }
+]);
